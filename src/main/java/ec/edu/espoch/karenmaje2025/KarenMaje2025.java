3b0859a62d78f7c61ecf5981d5b8a4cadc963b3c
Karen-404 <tat.3012.mc@gmail.com>
2024-12-05 13:51:52 -0500

    Primer comando

---
+++ src/main/java/ec/edu/espoch/karenmaje2025/KarenMaje2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.karenmaje2025;
+
+/**
+ *
+ * @author SO-LAB1-PC24
+ */
+public class KarenMaje2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
